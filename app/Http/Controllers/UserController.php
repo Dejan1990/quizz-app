360a7a2a5d0afd268eddafddc103ab6389a06cd6
dejan1990 <dejan.simo@hotmail.com>
2021-05-05 16:26:14 +0200

    Update user details

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -64,7 +64,8 @@ class UserController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = (new User)->findUser($id);
+        return view('backend.user.edit',compact('user'));
     }
 
     /**
@@ -76,7 +77,11 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request,[
+            'name'=>'required'
+        ]);
+        $user = (new User)->updateUser($request->all(),$id);
+        return redirect()->route('user.index')->with('message','user updated successfully!');
     }
 
     /**

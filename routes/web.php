8542f14cdf74c7819e99164c01d3779a6f3f2958
dejan1990 <dejan.simo@hotmail.com>
2021-05-05 15:56:40 +0200

    Register a user

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QuizController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,5 +27,6 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::resource('quiz', QuizController::class);
 Route::resource('question', QuestionController::class);
+Route::resource('user', UserController::class);
 
 Route::get('/quiz/{id}/questions', [QuizController::class, 'question'])->name('quiz.question');
